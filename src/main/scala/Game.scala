b3189567e59ec9d192a01c80e04251938ba83fca
Boshen Chen <boshenc@gmail.com>
2014-01-02 18:28:14 +1300

    A 3D cube that changes color while moving

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import org.lwjgl.BufferUtils
 import org.lwjgl.LWJGLException
 import org.lwjgl.opengl.Display
@@ -20,8 +21,11 @@ class Game {
                         #version 330
 
                         layout(location = 0) in vec4 position;
+
                         uniform float loopDuration;
                         uniform float time;
+
+                        uniform mat4 perspectiveMatrix;
 
                         void main()
                         {
@@ -29,12 +33,13 @@ class Game {
 
                             float currTime = mod(time, loopDuration);
                             vec4 totalOffset = vec4(
-                                cos(currTime * timeScale) * 0.5f,
-                                sin(currTime * timeScale) * 0.5f,
-                                0.0f,
-                                0.0f);
+                                    cos(currTime * timeScale) * 0.5f,
+                                    sin(currTime * timeScale) * 0.5f,
+                                    0.0f,
+                                    0.0f);
+                            vec4 cameraPos = position + totalOffset;
 
-                            gl_Position = position + totalOffset;
+                            gl_Position = perspectiveMatrix * cameraPos;
                         }
                         """
 
@@ -42,11 +47,9 @@ class Game {
                           #version 330
 
                           out vec4 outputColor;
-
                           uniform float fragLoopDuration;
                           uniform float time;
-
-                          const vec4 firstColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+                          const vec4 firstColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
                           const vec4 secondColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);
 
                           void main()
@@ -59,10 +62,54 @@ class Game {
                           """
 
   val vertexData = Array(
-    0.0f, 0.25f, 0.0f, 1.0f,
-    0.25f, -0.188f, 0.0f, 1.0f,
-    -0.25f, -0.188f, 0.0f, 1.0f
-  )
+    0.25f, 0.25f, -1.25f, 1.0f,
+    0.25f, -0.25f, -1.25f, 1.0f,
+    -0.25f, 0.25f, -1.25f, 1.0f,
+
+    0.25f, -0.25f, -1.25f, 1.0f,
+    -0.25f, -0.25f, -1.25f, 1.0f,
+    -0.25f, 0.25f, -1.25f, 1.0f,
+
+    0.25f, 0.25f, -2.75f, 1.0f,
+    -0.25f, 0.25f, -2.75f, 1.0f,
+    0.25f, -0.25f, -2.75f, 1.0f,
+
+    0.25f, -0.25f, -2.75f, 1.0f,
+    -0.25f, 0.25f, -2.75f, 1.0f,
+    -0.25f, -0.25f, -2.75f, 1.0f,
+
+    -0.25f, 0.25f, -1.25f, 1.0f,
+    -0.25f, -0.25f, -1.25f, 1.0f,
+    -0.25f, -0.25f, -2.75f, 1.0f,
+
+    -0.25f, 0.25f, -1.25f, 1.0f,
+    -0.25f, -0.25f, -2.75f, 1.0f,
+    -0.25f, 0.25f, -2.75f, 1.0f,
+
+    0.25f, 0.25f, -1.25f, 1.0f,
+    0.25f, -0.25f, -2.75f, 1.0f,
+    0.25f, -0.25f, -1.25f, 1.0f,
+
+    0.25f, 0.25f, -1.25f, 1.0f,
+    0.25f, 0.25f, -2.75f, 1.0f,
+    0.25f, -0.25f, -2.75f, 1.0f,
+
+    0.25f, 0.25f, -2.75f, 1.0f,
+    0.25f, 0.25f, -1.25f, 1.0f,
+    -0.25f, 0.25f, -1.25f, 1.0f,
+
+    0.25f, 0.25f, -2.75f, 1.0f,
+    -0.25f, 0.25f, -1.25f, 1.0f,
+    -0.25f, 0.25f, -2.75f, 1.0f,
+
+    0.25f, -0.25f, -2.75f, 1.0f,
+    -0.25f, -0.25f, -1.25f, 1.0f,
+    0.25f, -0.25f, -1.25f, 1.0f,
+
+    0.25f, -0.25f, -2.75f, 1.0f,
+    -0.25f, -0.25f, -2.75f, 1.0f,
+    -0.25f, -0.25f, -1.25f, 1.0f
+    )
 
   lazy val positionBufferObject = initVertexBuffer()
   lazy val theProgram = initProgram()
@@ -75,16 +122,15 @@ class Game {
     Display.destroy()
   }
 
-  def loop(lastFPS: Long, fps: Int) {
-    if (!Display.isCloseRequested) {
-      display()
+  @tailrec final def loop(lastFPS: Long, fps: Int) {
       val (newLastFPS, newFPS) = updateFPS(lastFPS, fps)
+      display()
       Display.update()
       Display.sync(100)
       if (Display.wasResized)
         reshape(Display.getWidth, Display.getHeight)
-      loop(newLastFPS, newFPS)
-    }
+      if (!Display.isCloseRequested)
+        loop(newLastFPS, newFPS)
   }
 
   def initWindow(width: Int, height: Int) {
@@ -107,6 +153,9 @@ class Game {
   def initGL() {
     val vao = GL30.glGenVertexArrays()
     GL30.glBindVertexArray(vao)
+    GL11.glEnable(GL11.GL_CULL_FACE)
+    GL11.glCullFace(GL11.GL_BACK)
+    GL11.glFrontFace(GL11.GL_CW)
   }
 
   def initProgram(): Int = {
@@ -118,10 +167,27 @@ class Game {
     val theProgram = createProgram(shaderList)
     val loopDurationUnf = GL20.glGetUniformLocation(theProgram, "loopDuration")
     val fragLoopDurUnf = GL20.glGetUniformLocation(theProgram, "fragLoopDuration")
+    val perspectiveMatrixUnif = GL20.glGetUniformLocation(theProgram, "perspectiveMatrix")
+
+    val frustumScale = 1.0f
+    val zNear = 0.5f
+    val zFar = 3.0f
+
+    val theMatrix = Array.ofDim[Float](16)
+    theMatrix(0) = frustumScale
+    theMatrix(5) = frustumScale
+    theMatrix(10) = (zFar + zNear) / (zNear - zFar)
+    theMatrix(14) = (2 * zFar * zNear) / (zNear - zFar)
+    theMatrix(11) = -1.0f
+
+    val theMatrixBuffer = BufferUtils.createFloatBuffer(theMatrix.length)
+    theMatrixBuffer.put(theMatrix)
+    theMatrixBuffer.flip()
 
     GL20.glUseProgram(theProgram)
     GL20.glUniform1f(loopDurationUnf, 5.0f)
     GL20.glUniform1f(fragLoopDurUnf, 10.0f)
+    GL20.glUniformMatrix4(perspectiveMatrixUnif, false, theMatrixBuffer)
     GL20.glUseProgram(0)
     shaderList.foreach(GL20.glDeleteShader(_))
     theProgram
@@ -174,13 +240,18 @@ class Game {
   def display() {
     GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
     GL11.glClear(GL11.GL_COLOR_BUFFER_BIT)
+
     GL20.glUseProgram(theProgram)
+
     val elapsedTimeUnf = GL20.glGetUniformLocation(theProgram, "time")
+
     GL20.glUniform1f(elapsedTimeUnf, getElapsedTime() / 1000.0f)
+
     GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, positionBufferObject)
     GL20.glEnableVertexAttribArray(0)
     GL20.glVertexAttribPointer(0, 4, GL11.GL_FLOAT, false, 0, 0)
-    GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3)
+    GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 36)
+
     GL20.glDisableVertexAttribArray(0)
     GL20.glUseProgram(0)
   }
